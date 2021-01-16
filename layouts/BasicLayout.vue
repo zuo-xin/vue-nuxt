bd810485bfeb2795e07fb39dd07e436aa4b6a3a3
moses.zuo <moses.zuo@feisu.com>
2021-01-16 11:07:05 +0800

    layout scss

---
+++ layouts/BasicLayout.vue
@@ -0,0 +1,23 @@
+<template>
+	<div class="basic-layout">
+		<div class="header">header</div>
+		<Nuxt />
+		<div class="footer">footer</div>
+	</div>
+</template>
+
+
+<style lang="scss" scoped>
+.basic-layout{
+	.header{
+		background: #f7f7f7;
+		color: #000;
+		font-size: 30px;
+	}
+	.footer{
+		background: #f7f7f7;
+		color: #000;
+		font-size: 30px;
+	}
+}
+</style>
