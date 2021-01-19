3e30c5a09bebe87f785e8e7b6fb1748e0799e30d
moses.zuo <moses.zuo@feisu.com>
2021-01-19 15:52:18 +0800

    nuxt link 跳转

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,45 +1,37 @@
 export default {
   // Global page headers (https://go.nuxtjs.dev/config-head)
   head: {
-    title: 'vue-nuxt',
+    title: "vue-nuxt",
     meta: [
-      { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: '' }
+      { charset: "utf-8" },
+      { name: "viewport", content: "width=device-width, initial-scale=1" },
+      { hid: "description", name: "description", content: "" }
     ],
-    link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
-    ]
+    link: [{ rel: "icon", type: "image/x-icon", href: "/favicon.ico" }]
   },
+  loading: false,
   server: {
     port: 2021, // default: 3000
     host: "0.0.0.0" // default: localhost
   },
   styleResources: {
-    scss: ['~/assets/scss/variable.scss']
+    scss: ["~/assets/scss/variable.scss"]
   },
   // Global CSS (https://go.nuxtjs.dev/config-css)
-  css: [
-    '~assets/iconfont/iconfont.css','~assets/scss/reset.scss'
-  ],
+  css: ["~assets/iconfont/iconfont.css", "~assets/scss/reset.scss"],
 
   // Plugins to run before rendering page (https://go.nuxtjs.dev/config-plugins)
-  plugins: [
-  ],
+  plugins: [],
 
   // Auto import components (https://go.nuxtjs.dev/config-components)
   components: true,
 
   // Modules for dev and build (recommended) (https://go.nuxtjs.dev/config-modules)
-  buildModules: [
-  ],
+  buildModules: [],
 
   // Modules (https://go.nuxtjs.dev/config-modules)
-  modules: [
-    '@nuxtjs/style-resources' 
-  ],
+  modules: ["@nuxtjs/style-resources",'@nuxtjs/component-cache'],
 
   // Build Configuration (https://go.nuxtjs.dev/config-build)
-  build: {
-  }
-}
+  build: {}
+};
--- pages/index.vue
+++ pages/index.vue
@@ -1,40 +1,64 @@
 <template>
-  <div class="container">
-    <div>
-      <Logo />
-      <h1 class="title">
-        Networking
-      </h1>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          rel="noopener noreferrer"
-          class="button--green"
-        >
-          Documentation
-        </a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          rel="noopener noreferrer"
-          class="button--grey"
-        >
-          GitHub
-        </a>
-      </div>
-    </div>
-  </div>
+	<div class="container">
+		<div>
+			<Logo />
+      <NuxtLink to="/list" >to list</NuxtLink>
+			<h1 class="title">
+				Networking
+			</h1>
+			<div class="links">
+				<a href="https://nuxtjs.org/" target="_blank" rel="noopener noreferrer" class="button--green">
+					Documentation
+				</a>
+				<a href="https://github.com/nuxt/nuxt.js" target="_blank" rel="noopener noreferrer" class="button--grey">
+					GitHub
+				</a>
+			</div>
+		</div>
+
+		<h2>Users</h2>
+		<ul class="users">
+			<li v-for="user in users" :key="user.id">
+				<NuxtLink :to="{name:'users-id',params: { id: user.id }}">
+					{{ user.name }}
+				</NuxtLink>
+			</li>
+		</ul>
+
+    <button type="button" @click.stop="click" class="login">click to login</button>
+	</div>
 </template>
 
 <script>
+
+
+import axios from 'axios'
+
 export default {
-  layout: 'BasicLayout'
+	layout: 'BasicLayout',
+	async asyncData () {
+		const { data } = await axios.get('https://jsonplaceholder.typicode.com/users')
+		return { users: data }
+  },
+  mounted(){
+    console.log(process)
+    console.log(process.client)
+  },
+  methods:{
+    click(){
+      let routeData = this.$router.resolve({name: 'account-login'});
+      window.open(routeData.href, '_blank');
+    }
+  }
 }
 </script>
 
 <style lang="scss">
-.title{
-  color: $btnBgColor1;
+.title {
+	color: $btnBgColor1;
+}
+.login{
+  color: blue;
+  cursor: pointer;
 }
 </style>
